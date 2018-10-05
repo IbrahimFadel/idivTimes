dd0262fba09ddc7f4fd36a515ec9e51ffaff08c2
IbrahimFadel <ibrahim.fadel@ucc.on.ca>
2018-10-04 21:42:34 -0400

    small change

--- server.js
+++ server.js
@@ -2,8 +2,51 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+var phpExpress = require('php-express')({
+  binPath: 'php'
+});
+
 app.get('/', (req, res, next) => {
 	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/index.html', (req, res, next) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/index.css', (req, res, next) => {
+	res.sendFile(__dirname + '/index.css');
+});
+
+app.get('/css/bootstrap.min.css', (req, res, next) => {
+	res.sendFile(__dirname + '/css/bootstrap.min.css');
+});
+
+app.get('/src/img/github.png', (req, res, next) => {
+	res.sendFile(__dirname + '/src/img/github.png');
+});
+
+app.get('/contact.html', (req, res, next) => {
+	res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/staff.html', (req, res, next) => {
+	res.sendFile(__dirname + '/staff.html');
+});
+
+app.get('/catalog.html', (req, res, next) => {
+	res.sendFile(__dirname + '/catalog.html');
+});
+
+app.get('/action_page.php', (req, res, next) => {
+	res.sendFile(__dirname + '/action_page.php');
+});
+
+app.post('/contact.html',function(req,res){
+   var username = req.body.username;
+   var htmlData = 'Hello:' + username;
+   res.send(htmlData);
+   console.log(htmlData);
 });
 
 app.listen(PORT, () => {
