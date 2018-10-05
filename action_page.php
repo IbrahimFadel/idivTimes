a1716211eda7023d34db02a23f5b08fcf51b4ee9
IbrahimFadel <ibrahim.fadel@ucc.on.ca>
2018-10-05 10:54:58 -0400

    All files have get requests with the new express server

--- action_page.php
+++ action_page.php
@@ -7,26 +7,8 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-primary">
-  <a class="navbar-brand text-light" href="/index.html">The Idiv Times</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<?php echo '<p>Hi</p>'; ?>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link text-light" href="/catalog.html">Catalog <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light" href="/staff.html">Staff</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light" href="/contact.html">Contact</a>
-      </li>
-    </ul>
-  </div>
-</nav>
 
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const { body,validationResult } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
+const php = require('php-express');
 
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -52,6 +53,50 @@ app.get('/catalog.html', (req, res, next) => {
 app.get('/action_page.php', (req, res, next) => {
 	res.sendFile(__dirname + '/action_page.php');
 });
+
+app.get('/src/img/no-image.png', (req, res, next) => {
+	res.sendFile(__dirname + '/src/img/no-image.png');
+});
+
+app.get('/src/img/no-profile.png', (req, res, next) => {
+	res.sendFile(__dirname + '/src/img/no-profile.png');
+});
+
+app.get('/src/articles/png/article1.png', (req, res, next) => {
+	res.sendFile(__dirname + '/src/articles/png/article1.png');
+});
+
+app.get('/src/articles/png/article2.png', (req, res, next) => {
+	res.sendFile(__dirname + '/src/articles/png/article2.png');
+});
+
+app.get('/src/articles/png/article3.png', (req, res, next) => {
+	res.sendFile(__dirname + '/src/articles/png/article3.png');
+});
+
+app.get('/src/fonts/NexaBold.otf', (req, res, next) => {
+	res.sendFile(__dirname + '/src/fonts/NexaBold.otf');
+});
+
+app.get('/src/fonts/NexaLight.otf', (req, res, next) => {
+	res.sendFile(__dirname + '/src/fonts/NexaLight.otf');
+});
+
+app.get('/src/articles/pdf/article1.pdf', (req, res, next) => {
+	res.sendFile(__dirname + '/src/articles/pdf/article1.pdf');
+}); 
+
+app.get('/src/articles/html/article2.html', (req, res, next) => {
+	res.sendFile(__dirname + '/src/articles/html/article2.html');
+}); 
+
+app.get('/src/articles/pdf/article3.pdf', (req, res, next) => {
+	res.sendFile(__dirname + '/src/articles/pdf/article3.pdf');
+}); 
+
+app.get('/src/articles/pdf/article2.pdf', (req, res, next) => {
+	res.sendFile(__dirname + '/src/articles/pdf/article2.pdf');
+}); 
 
 app.post('/action_page.php',function(req,res){
    var username = req.body.name;
