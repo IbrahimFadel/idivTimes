d68f71ce15bf228850f11de8ac7a1deda1ff4c07
IbrahimFadel <ibrahim.fadel@ucc.on.ca>
2018-10-10 00:35:04 -0400

    form working like a charm, prettied up json file, login needs lots of work!

---
+++ idivTimes/README.md
@@ -0,0 +1,18 @@
+# idivTimes
+A website to view the idiv times paper online
+
+# Setting this up:
+
+Our Dependancies:
+
+* Express
+
+Version: 4.16.3
+
+* php-express
+
+Version: 0.0.3
+
+Install all dependancies:
+
+npm install
---
+++ idivTimes/action_page.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Idiv Times test - Signing Up</title>
+
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="./index.css" rel="stylesheet">
+</head>
+<body>
+
+<?php
+ echo "Hi"; 
+ ?>
+
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
